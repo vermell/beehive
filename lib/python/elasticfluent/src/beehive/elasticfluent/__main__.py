43639072b35da4cc5308488afadaa9b644748f09
Michael Ullrich <mullrichael@gmail.com>
2021-10-24 13:43:01 +0200

    refactor helloworld

--- lib/python/elasticfluent/src/beehive/elasticfluent/__main__.py
+++ lib/python/elasticfluent/src/beehive/elasticfluent/__main__.py
@@ -1,6 +1,11 @@
-from beehive.elasticfluent.foo import foo
+import time
 
 # This comment is too longa # This comment is too longa# This comment is too
 # lona ie ga# This comment is too longa# This
-print("haha")
-print(foo(20))
+
+if __name__ == "__main__":
+    counter = 0
+    while True:
+        counter += 1
+        print(f"Hello world-service is running [{counter}]")
+        time.sleep(10)
